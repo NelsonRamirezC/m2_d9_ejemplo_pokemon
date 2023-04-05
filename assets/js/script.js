a3fded190b7db45f246c049f8707bb368102e93f
Nelson Ramirez <xhyskoax@gmail.com>
2023-04-05 10:55:48 -0400

    comenzando a incluir eventos

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+let tituloPrincipal = document.getElementById("tituloPrincipal");
+
+tituloPrincipal.innerHTML = "<strong>Guía Pokémon</strong>"
+
+
+function cambiarColor(elemento, color){
+    elemento.style.color = color;
+}
+
+function quitarColor(elemento){
+    elemento.style.color = "black";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+let subtitulosTag = document.getElementsByTagName("h2")
+let subtitulos = document.querySelectorAll("h2");
+
+let footer = document.querySelector("footer");
+console.log(footer)
+footer.innerHTML = "<h2>Es un subtitulo</h2>"
+
+
+console.log(subtitulosTag)
+console.log(subtitulos)
+ */
